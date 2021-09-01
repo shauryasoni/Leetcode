a00f5391d1906dd1921ca8eb21245a4fbb60c5f2
shauryasoni <shaurya.soni@gmail.com>
2021-09-01 17:50:30 +0530

    Accepted

--- surroundedRegions.py
+++ surroundedRegions.py
@@ -1,25 +1,24 @@
-from typing import List
 class Solution:
     
     def os(self,board,visited,group,r,c,i,j) :
-        
+        if board[i][j] == "X" :
+            return True
         if i == 0 or i == r-1 :
             return False
         if j == 0 or j == c-1 :
             return False
-        if board[i][j] == "1" :
-            return False
+        
         if (i,j) in visited :
             return True
         elif board[i][j] == "O":
             visited.add((i,j))
             group.append((i,j))
-            r = self.os(board,visited,group,r,c,i,j+1)
+            ri = self.os(board,visited,group,r,c,i,j+1)
             l = self.os(board,visited,group,r,c,i,j-1)
             u = self.os(board,visited,group,r,c,i-1,j)
             d = self.os(board,visited,group,r,c,i+1,j)
         
-            return True or r or l or u or d
+            return True and ri and l and u and d
         
         
     def solve(self, board: List[List[str]]) -> None:
@@ -38,7 +37,10 @@ class Solution:
                     corno = self.os(board,visited,group,r,c,i,j)
                     if corno == True :
                         captured.append(group)
-                        print(group)
+       # print(captured)
+        for i in captured :
+            for j in i :
+                board[j[0]][j[1]] = "X"
         
         
-print(Solution().solve([["X","X","X","X"],["X","O","X","X"],["X","X","O","X"],["X","O","X","X"]]) )
+        
