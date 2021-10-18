fe671da6f41d573de4c14ee0511a597091d7fce9
shauryasoni <shaurya.soni@gmail.com>
2021-10-18 12:07:56 +0530

    Leetcode

--- graphs_trees/rightSideTreeView.py
+++ graphs_trees/rightSideTreeView.py
@@ -1,9 +1,9 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
 from typing import List
 class Solution:
     def bfs(self,ans,frontier):
@@ -25,7 +25,7 @@ class Solution:
         
         
         
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    def rightSideView(self, root:TreeNode) -> List[int]:
         if root == None :
             return []
         
